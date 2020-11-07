fc0ad20c57797b9910d4510f010e082f82f9eaa4
Rogerio Martins <rmaviador@gmail.com>
2020-11-06 22:49:06 -0300

    :zap: ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
 
     var header = document.querySelector("header");
-    
+
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // EVENTO DE CLICK PARA MENU EM DISPOSITIVOS MENORES
 
-function  menuToggle () {
+function menuToggle() {
     let menuArea = document.getElementById('menu-area');
 
-    if(menuArea.style.width == '230px'){
+    if (menuArea.style.width == '230px') {
         menuArea.style.width = '0px';
-    }else {
+    } else {
         menuArea.style.width = '230px';
     }
 
